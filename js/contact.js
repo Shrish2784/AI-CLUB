1432ad5da4f59932aa4a9b1043e359202cc1ad68
shrish2784 <shrishasthana@outlook.com>
2018-10-03 22:44:22 +0530

    Contact us Ready.

--- js/contact.js
+++ js/contact.js
@@ -2,3 +2,13 @@ let header = document.querySelector("header");
 if (window.location.pathname === "/contact.html") {
   header.style.backgroundColor = "rgba(255, 255, 255, 1)";
 }
+// let students = document.querySelectorAll(".student");
+// students.forEach(student => {
+//   student.addEventListener("hover", (e) => {
+//     console.log("HOVER");
+//     let live = document.getElementById("live");
+//     let hidden = document.getElementById("hidden");
+//     live.style.display = "none";
+//     hidden.style.display = "block";
+//   })
+// })
