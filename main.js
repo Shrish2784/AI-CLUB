c8e7bb0e2535a410e39993d892dbb7ebd6b9d5cf
shrish2784 <shrishasthana@outlook.com>
2018-10-03 16:16:00 +0530

    Init Commit

---
+++ main.js
@@ -0,0 +1,9 @@
+document.addEventListener("scroll", (e) => {
+    let scrollAmount = window.pageYOffset || document.documentElement.scrollTop;
+    let header = document.querySelector("header");
+    if (scrollAmount >= 700) {
+        header.style.backgroundColor = "rgba(255, 255, 255, 1)";
+    } else {
+        header.style.backgroundColor = "rgba(255, 255, 255, 0.7)";
+    }
+})
